2491e9859a90513f46f331768f7f676e37f08d34
Raden Parhanudin <radenparhanudin@gmail.com>
2020-02-24 22:55:14 +0800

    Install Package yang dibutuhkan

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // $user = User::find(Auth::user()->id);
+        // $roles = $user->getRoleNames(); // Returns a collection
+        // return $roles;
+        // $user = new User;
+        // return $user->allOnline();
+        return view('home');
+    }
+}
